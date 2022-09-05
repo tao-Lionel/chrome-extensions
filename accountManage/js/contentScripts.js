51f4b54408acd5d448b13f1043593522edb74804
wangtao <lionel_tao@163.com>
2022-09-05 11:39:22 +0800

    完善记住账号功能

--- accountManage/js/contentScripts.js
+++ accountManage/js/contentScripts.js
@@ -2,14 +2,65 @@
 // chrome.storage.sync.get(['info'], (obj) => {
 //   sendTargets = JSON.parse(obj.info)
 // })
-
+// import Cookies from './js.cookie.js'
 
 $(function () {
+  async function sendSms() {
+    var url = '/gateway/main/sys/sms/sendSms'
+    var options = {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        mobile: "",
+        smsType: 3
+      })
+    }
+    try {
+      let res = await fetch(url, options)
+      console.log(res);
+    } catch (error) {
+      console.log('Request Failed', error);
+    }
+  }
+  async function login() {
+    var url = '/gateway/main/uc/sso/loginFirstStep'
+    var options = {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        app: 2,
+        domain: "",
+        platform: 1,
+        username: "",
+        verifyCode: "1234",
+      })
+    }
+    try {
+      let res = await fetch(url, options)
+      let data = await res.json()
+      const cookieParams = { domain: window.location.hostname, secure: false }
+      Cookies.set('xhs_token', data.data.tokenData.token, cookieParams)
+      Cookies.set('xhs_refresh_token', data.data.tokenData.refreshToken, cookieParams)
 
-  console.log('window.location.href', window.location.port);
-  $('input[placeholder="请输入手机号"]').val('13821952053')
-  $('input[placeholder="请输入验证码"]').val('1234')
-  setTimeout(() => {
-    $('button[class="ant-btn ant-btn-link x-button x-button__link"]').click()
-  }, 500)
+      if (Cookies.get('xhs_token') && Cookies.get('xhs_refresh_token')) {
+        // 根据端口号跳页面
+        let url = '/my/first/index'
+        if (window.location.port === "3004") {
+          url = '/work/index'
+        }
+        window.open(url, '_self')
+      }
+    } catch (error) {
+      console.log('Request Failed', error);
+    }
+  }
+
+  if (window.location.pathname === '/login') {
+    sendSms()
+    login()
+  }
 });
--- accountManage/js/popup.js
+++ accountManage/js/popup.js
@@ -1,63 +1,63 @@
-var sendTargets = [
-    {
-        projectName: "有料云",
-        webhook: "",
-    },
-    {
-        projectName: "西红市",
-        webhook: "",
-    },
-    {
-        projectName: "有料网",
-        webhook: "",
-    },
-    {
-        projectName: "线上问题汇总",
-        webhook: "",
-    },
-];
+// var sendTargets = [
+//     {
+//         projectName: "有料云",
+//         webhook: "",
+//     },
+//     {
+//         projectName: "西红市",
+//         webhook: "",
+//     },
+//     {
+//         projectName: "有料网",
+//         webhook: "",
+//     },
+//     {
+//         projectName: "线上问题汇总",
+//         webhook: "",
+//     },
+// ];
 
-for (var i = 0; i < sendTargets.length; i++) {
-    var text = "<span>项目名称(该项目名称务必和TAPD项目名称保持一致)：</span> ";
-    var text1 = `<input id='projectName${i}' type = 'text' disabled value='${sendTargets[i].projectName}'/>`;
-    var text2 = "<span>webHook地址：</span>";
-    var text3 = `<input id='webHook${i}' type='text' disabled/>`;
-    $(`#sendTarget${i}`).append(text, text1, text2, text3);
-}
-
-//编辑
-$("#btnEdit").click(e => {
-    $("input").removeAttr("disabled");
-    $("#btnSave").removeClass("none");
-    $(e.target).attr("class", "none");
-});
-
-// 获取当前tab标签
-// async function getCurrentTab() {
-//     let queryOptions = { active: true, currentWindow: true };
-//     let [tab] = await chrome.tabs.query(queryOptions);
-//     return tab;
+// for (var i = 0; i < sendTargets.length; i++) {
+//     var text = "<span>项目名称(该项目名称务必和TAPD项目名称保持一致)：</span> ";
+//     var text1 = `<input id='projectName${i}' type = 'text' disabled value='${sendTargets[i].projectName}'/>`;
+//     var text2 = "<span>webHook地址：</span>";
+//     var text3 = `<input id='webHook${i}' type='text' disabled/>`;
+//     $(`#sendTarget${i}`).append(text, text1, text2, text3);
 // }
 
-// 保存
-$("#btnSave").click(async e => {
-    $("input").attr("disabled", "true");
-    $(e.target).attr("class", "none");
-    $("#btnEdit").removeClass("none");
-    // 保存数据
-    let info = [];
-    for (var i = 0; i < sendTargets.length; i++) {
-        info.push({
-            projectName: $(`#projectName${i}`).val(),
-            webHook: $(`#webHook${i}`).val(),
-        });
+// //编辑
+// $("#btnEdit").click(e => {
+//     $("input").removeAttr("disabled");
+//     $("#btnSave").removeClass("none");
+//     $(e.target).attr("class", "none");
+// });
 
-        // info[`projectName${i}`] = $(`#projectName${i}`).val();
-        // info[`webHook${i}`] = $(`#webHook${i}`).val();
-    }
-    chrome.storage.sync.set({ info: JSON.stringify(info) });
+// // 获取当前tab标签
+// // async function getCurrentTab() {
+// //     let queryOptions = { active: true, currentWindow: true };
+// //     let [tab] = await chrome.tabs.query(queryOptions);
+// //     return tab;
+// // }
 
-    // const tab = await getCurrentTab();
-    // console.log(tab);
-    // await chrome.tabs.sendMessage(tab.id, { msg: "msg" });
-});
+// // 保存
+// $("#btnSave").click(async e => {
+//     $("input").attr("disabled", "true");
+//     $(e.target).attr("class", "none");
+//     $("#btnEdit").removeClass("none");
+//     // 保存数据
+//     let info = [];
+//     for (var i = 0; i < sendTargets.length; i++) {
+//         info.push({
+//             projectName: $(`#projectName${i}`).val(),
+//             webHook: $(`#webHook${i}`).val(),
+//         });
+
+//         // info[`projectName${i}`] = $(`#projectName${i}`).val();
+//         // info[`webHook${i}`] = $(`#webHook${i}`).val();
+//     }
+//     chrome.storage.sync.set({ info: JSON.stringify(info) });
+
+//     // const tab = await getCurrentTab();
+//     // console.log(tab);
+//     // await chrome.tabs.sendMessage(tab.id, { msg: "msg" });
+// });
